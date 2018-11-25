e033a3ce02f167222e28fc443d1f08a2b34ccbc2
Gael Schlupp <gael.schlupp@gmail.com>
2018-11-25 18:06:02 +0100

    Collision detection

---
+++ Assets/selectedCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class selectedCharacter : MonoBehaviour {
+
+    Image sprite;
+
+    void Start()
+    {
+        sprite = GetComponent<Image>();
+    }
+
+	// prends le trésor et manipule le
+	// si la couleur est changé, ne la change pas (loop to do)
+	void Update(){
+		updateColor(Test_game_manager.instance.selectedPlayer);
+	}
+
+    void updateColor(Color color){
+        sprite.CrossFadeColor(color,0,false,false);
+		Debug.Log("n'importe quoi");
+    }
+}
--- Assets/test_collision.cs
+++ Assets/test_collision.cs
@@ -1,7 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class test_collision : MonoBehaviour 
 {
+    Image sprite;
+
+    void Start()
+    {
+        sprite = GetComponent<Image>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        updateColor(Color.red);
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        updateColor(Color.blue);
+    }
+
+    // je créé une fonction avec paramètre couleur (color) et je prends un Image (sprite) et j'utilise la méthode (CrossFadeColor).
+    // je vais dans le script GameManager.cs, je accède à l'instance de la classe et j'utilise la méthode.
+    void updateColor(Color color){
+        sprite.CrossFadeColor(color,0,false,false);
+        Test_game_manager.instance.updateSelectedPlayer(color);
+    }
 }
